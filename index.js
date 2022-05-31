eb2347e1268e7a4e525a40858a25480a42ca2133
Facundo Quintana <facunquintana@gmail.com>
2022-05-31 12:25:01 -0300

    preference

--- index.js
+++ index.js
@@ -98,7 +98,7 @@ app.post('/create', async function(req, res) {
     console.error("Error adding document: ", error);
     });
 
-    res.send('partido creado');
+    
 })
 
 app.post('/setActivo', async function(req, res) {
@@ -157,6 +157,92 @@ let preference = {
     .catch(function (error) {
       console.log(error);
     });
+})
+
+app.post('/create2', async function(req, res) {
+
+  Date.prototype.addHours = function(h) {
+      this.setTime(this.getTime() + (h*60*60*1000));
+      return this;
+    }
+
+  var date = new Date();
+  
+  //sumarle una hora
+  //var date2 = new Date().addHours(1)
+
+  var now = new Date();
+  now.setMinutes(now.getMinutes() + 10); // timestamp
+  now = new Date(now); // Date object
+
+  var idCancha = req.body.idCancha;
+  var nombreCancha = req.body.nombreCancha;
+  var estado = req.body.estado;
+  var emailUsuario = req.body.emailUsuario;
+  var fechaInicio = date;
+  var fechaFinn = now;
+  var idCamara = req.body.idCamara;
+  var urlVideo = req.body.urlVideo;
+  var hashMercadopago = req.body.hashMercadopago;
+
+
+  await db.collection('partidos').add({
+      idCancha,
+      nombreCancha,
+      estado,
+      emailUsuario,
+      fechaInicio,
+      fechaFinn,
+      idCamara,
+      urlVideo,
+      hashMercadopago
+  })
+
+  // Add a new document with a generated id.
+  db.collection("partidos").add({
+      idCancha,
+      nombreCancha,
+      estado,
+      emailUsuario,
+      fechaInicio,
+      fechaFinn,
+      idCamara,
+      urlVideo,
+      hashMercadopago
+      })
+  .then(function(docRef) {
+  console.log("Document written with ID: ", docRef.id);
+
+  // Crea un objeto de preferencia
+  let preference = {
+    items: [
+      {
+        title: "Mi producto",
+        unit_price: 100,
+        quantity: 1,
+      },
+    ],
+    external_reference: docRef.id
+  };
+  
+  mercadopago.preferences
+    .create(preference)
+    .then(function (response) {
+      console.log(response);
+    res.redirect(response.body.init_point);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+
+  
+  })
+  .catch(function(error) {
+  console.error("Error adding document: ", error);
+  });
+
+
+
 })
 
 app.listen(PORT, function () {
